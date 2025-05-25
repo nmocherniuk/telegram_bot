af8734e8b93b8d0443953116a91d4c855ed187c5
nmocherniuk <nazar.mocherniuk@gmail.com>
2025-05-26 01:03:36 +0300

    refactor time zone

--- bot.py
+++ bot.py
@@ -2,11 +2,14 @@ import asyncio
 import os
 from telegram import Bot
 from datetime import datetime
+from zoneinfo import ZoneInfo  # <-- нове
 
 TOKEN = os.getenv("TOKEN")
 USER_ID = int(os.getenv("USER_ID"))
 
 bot = Bot(token=TOKEN)
+
+KYIV_TZ = ZoneInfo("Europe/Kyiv")
 
 async def send_reminder():
     message = "🔥 Don't forget to send a message on TikTok today!"
@@ -15,7 +18,7 @@ async def send_reminder():
     except Exception as e:
         print(f"Error sending message: {e}")
 
-SEND_HOURS = [18, 22]
+SEND_HOURS = [21]
 MINUTE_TO_SEND = 0
 
 already_sent = set()
@@ -23,14 +26,14 @@ already_sent = set()
 async def main():
     print("Bot started...")
     while True:
-        now = datetime.now()
+        now = datetime.now(KYIV_TZ)
         current_time = (now.hour, now.minute)
 
         if now.minute == MINUTE_TO_SEND and now.hour in SEND_HOURS:
             if current_time not in already_sent:
                 await send_reminder()
                 already_sent.add(current_time)
-                print(f"Reminder sent at {now.hour:02d}:{now.minute:02d}")
+                print(f"Reminder sent at {now.hour:02d}:{now.minute:02d} (Kyiv time)")
         else:
             if len(already_sent) == len(SEND_HOURS):
                 already_sent.clear()
